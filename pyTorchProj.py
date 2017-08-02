e0935f100cac233aaa73838a7a924515a8fabd6f
Yonatan Lifshitz <yonlif@gmail.com>
2017-08-02 15:33:42 +0300

    minor update

--- pyTorchProj.py
+++ pyTorchProj.py
@@ -17,20 +17,32 @@ counter = 0
 page = re.sub('\s\s+',' ',page)
 page = page.split('\n')
 del page[-1]
-
 for el in page:
     tmp = el.split(' ')
     if (tmp[3] != '?'):
         tmp[7] = tmp[7][0]
         tmp = tmp[:8]
         tmp[4] = tmp[4][:4]
-        r = []
-        for a in tmp:
-            r.append(float(a))
-        data.append([r[0], r[1:]])
+        for a in range(0,8):
+            tmp[a] = float(tmp[a])
+        data.append(tmp)
+data = torch.Tensor(data)
 
+print("Data:")
 print(data)
 print(len(data))
+
+divData = torch.Tensor(8)
+meanData = torch.Tensor(8)
+for a in range(0,8):
+    num = torch.mean(data[:,a])
+    data[:,a] -= num
+    meanData[a] = num
+    num = torch.std(data[:,a])
+    data[:,a] /= num
+    divData[a] = num
+
+print(data)
 
 def disp(W,b):
     outString = "y = "
@@ -38,19 +50,31 @@ def disp(W,b):
         outString += (str(W[i]) + " * X" + str(i + 1) + " + ")
     outString += str(b).replace("\n"," ")
     return outString
-batch_size = 4
-def get_batch(count):
 
+batch_size = 4
+'''
+def get_batch(count):
     x = torch.Tensor(batch_size,7)
     y = torch.Tensor(batch_size,1)
     for i in range(0, batch_size):
         y[i] = data[count + i][0]
         x[i] = torch.Tensor(data[count + i][1])
     return Variable(x), Variable(y)
+'''
+
+def get_batch(count):
+    x = data[count:count+batch_size, 1:]
+    y = data[count:count+batch_size, 0]
+    return Variable(x), Variable(y)
 
 fc = torch.nn.Linear(7, 1)
-loss = 0
 
+print("Weights:")
+print(fc.weight)
+
+numberOfBatches = 80
+epoc = 0
+numberOfEpocs = 5
 counter = 0
 while True:
     # Get data
@@ -61,24 +85,36 @@ while True:
     fc.zero_grad()
 
     # Forward pass
-    h = fc(batch_x) - batch_y
-
-    for a in h:
-        a = a * a
-
-    output = torch.mean(h)
-    loss = output
+    loss = torch.mean((fc(batch_x) - batch_y).pow(2))
 
     # Backward pass
-    output.backward()
+    loss.backward()
 
     # Apply gradients
     for param in fc.parameters():
         param.data.add_(-0.01 * param.grad.data)
+    '''
+    # AdaGrad update
+    countHelp = 0
+    for param in fc.parameters():
+        cache[countHelp] += param.grad.data * param.grad.data
+        param.data.add_(-0.01 * param.grad.data / (torch.sqrt(cache[countHelp]) + 1e-7))
+        countHelp += 1
+    '''
 
     # Stop criterion
-    if loss < 1e-3 or counter == 320 :
-        break
+    if counter == numberOfBatches*batch_size:
+        counter = 0
+        epoc += 1
+        print("Loss in epoc " + str(numberOfEpocs) + ": " +str(loss))
+        if epoc == numberOfEpocs:
+            break
 
-print('Loss: ' + str(loss))
-print('==> Learned function: ' + disp(fc.weight.data.view(-1), fc.bias.data))
+a = loss[0]
+print(loss[0][0])
+print('Loss: ' + str(loss[0]) + " After " + str(numberOfBatches) + " batches")
+print('==> Learned function: ' + disp(fc.weight.data.view(-1), fc.bias.data.view(-1)))
+#help = []
+#for a in range(0,7):
+#    help.append(fc.weight.data.view(-1)[a] * divData[a] + meanData[a])
+#print('==> Actual function ' + disp(help, fc.bias.data))
